34dbc52339b9ce07b3ef38b88a8a99eedbf7d310
pavlosius <pavlosius@mail.ru>
2023-04-28 19:02:36 +0300

    Add project files.

---
+++ ConsoleApp_Exercise12.1.5_SkillFactory_Csh-dev/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp_Exercise12._1._5_SkillFactory_Csh_dev;
+
+User[] users = new User[]
+{
+    new User { Login="ali123", Name = "Alice", IsPremium = false },
+    new User { Login="bibi98", Name = "Bob", IsPremium = true },
+    new User { Login="rituhi76", Name = "Tom", IsPremium = false }
+};
+
+foreach (var user in users)
+{
+    GreetUser(user);
+}
+Console.ReadKey();
+
+void GreetUser(User user)
+{
+    Console.WriteLine($"Здравствуйте, {user.Name}!");
+    // Остановка на 1 с
+    Thread.Sleep(1000);
+    if (user.IsPremium != true)
+    {
+        ShowAds();
+    }
+}
+
+static void ShowAds()
+{
+    Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+    // Остановка на 1 с
+    Thread.Sleep(1000);
+
+    Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+    // Остановка на 2 с
+    Thread.Sleep(2000);
+
+    Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+    // Остановка на 3 с
+    Thread.Sleep(3000);
+}
---
+++ ConsoleApp_Exercise12.1.5_SkillFactory_Csh-dev/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Exercise12._1._5_SkillFactory_Csh_dev
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+    }
+}
